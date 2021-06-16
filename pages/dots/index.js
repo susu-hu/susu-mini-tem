b4ad6eeac0131a07172f5e9be0520ad43be17ac1
susu <1628469970@qq.com>
2021-06-16 11:00:09 +0800

    新增指示点

---
+++ pages/dots/index.js
@@ -0,0 +1,83 @@
+// pages/dots/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    autoplay: true, //是否自动切换
+    interval: 3000, //自动切换时间间隔,3s
+    duration: 1000, //  滑动动画时长1s
+    swiperCurrent:'',
+    goodsInfo:{
+      imgList:[
+        'https://i.postimg.cc/Bn1XpkSn/susu.jpg',
+        'https://i.postimg.cc/Bn1XpkSn/susu.jpg',
+        'https://i.postimg.cc/Bn1XpkSn/susu.jpg',
+        'https://i.postimg.cc/Bn1XpkSn/susu.jpg'
+      ]
+    }
+
+  },
+  swiperChange:function(e){
+    this.setData({
+      swiperCurrent:e.detail.current
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
