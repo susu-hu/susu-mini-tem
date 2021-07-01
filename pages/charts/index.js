cab986a91d696b3d325611c4a79cd2ef33c4a911
susu <1628469970@qq.com>
2021-07-01 14:21:22 +0800

    更新echarts的tooltip显示

--- pages/charts/index.js
+++ pages/charts/index.js
@@ -46,10 +46,42 @@ Page({
             itemWidth: 10,  // 设置宽度
             itemHeight: 10, // 设置高度
         },
+        
         tooltip: {
           show: true,
           trigger: 'item',
-          formatter: "{b} : \n {c}% "  
+          formatter: "{b} : \n {c}% "  ,
+          position: function (point, params, dom, rect, size) {
+            // 鼠标坐标和提示框位置的参考坐标系是：以外层div的左上角那一点为原点，x轴向右，y轴向下
+            // 提示框位置
+            let x = 0; // x坐标位置
+            let y = 0; // y坐标位置
+       
+            // 当前鼠标位置
+            let pointX = point[0];
+            let pointY = point[1];
+  
+       
+            // 提示框大小
+            let boxWidth = size.contentSize[0];
+            let boxHeight = size.contentSize[1];
+       
+            // boxWidth > pointX 说明鼠标左边放不下提示框
+            if (boxWidth > pointX) {
+              x = 5;
+            } else { // 左边放的下
+              x = pointX - boxWidth;
+            }
+       
+            // boxHeight > pointY 说明鼠标上边放不下提示框
+            if (boxHeight > pointY) {
+              y = 5;
+            } else { // 上边放得下
+              y = pointY - boxHeight;
+            }
+       
+            return [x, y];
+          }
         },
     
         series: [{
@@ -64,20 +96,20 @@ Page({
           radius: ['0%', '40%'],
           data: [{
             value: 55,
-            name: '北京',
+            name: '北京2qweqweqw',
             itemStyle: {color: '#9966ff'}
           }, {
             value: 20,
-            name: '武汉'
+            name: '武汉sdasdasd'
           }, {
             value: 10,
-            name: '杭州'
+            name: '杭州dasdaewqeqweqweqwsdas'
           }, {
             value: 20,
             name: '广州'
           }, {
             value: 38,
-            name: '上海'
+            name: '上海武汉sdasdasd'
           }],
           // itemStyle: {
           //   normal: {
