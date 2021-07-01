41a43648cc507d7db933b25414cd0935afc8368f
susu <1628469970@qq.com>
2021-07-01 18:16:21 +0800

    xx

--- pages/getStar/index.js
+++ pages/getStar/index.js
@@ -37,61 +37,48 @@ Page({
 
   },
   score:function(e){
-    var that=this;
-    for(var i=0;i<that.data.stars.length;i++){
-      var allItem = 'stars['+i+'].flag';
-      console.log(allItem)
-      that.setData({
-        [allItem]: 1
-      })
-    }
-    var index=e.currentTarget.dataset.index;
-    console.log(index)
-    for(var i=0;i<=index;i++){
-      var item = 'stars['+i+'].flag';
-      that.setData({
-        [item]:2
-      })
-      console.log(this.data.stars)
-    }
-
-    let a=this.data.stars.filter(item=>item.flag==2)
-    console.log(a.length)
-    this.setData({
-      chosedStar:6-a.length
-    })
-  },
-
-  scoreReview(e){
     this.data.stars.forEach(item=>{
       item.flag=1
       this.setData({
         stars:this.data.stars
       })
     })
-    // for(var i=0;i<this.data.stars.length;i++){
-    //   var allItem = 'stars['+i+'].flag';
-    //   console.log(allItem)
-    //   this.setData({
-    //     [allItem]: 1
-    //   })
-    // }
-    var index=4-e.currentTarget.dataset.index;
+    
+    var index=e.currentTarget.dataset.index;
     for(var i=0;i<=index;i++){
       var item = 'stars['+i+'].flag';
       this.setData({
         [item]:2
       })
-      console.log(this.data.stars)
     }
     let a=this.data.stars.filter(item=>item.flag==2)
-    console.log(a.length)
     this.setData({
       chosedStar:6-a.length
     })
-
-  
   },
+
+  // scoreReview(e){
+  // var that=this;
+    // for(var i=0;i<that.data.stars.length;i++){
+    //   var allItem = 'stars['+i+'].flag';
+    //   console.log(allItem)
+    //   that.setData({
+    //     [allItem]: 1
+    //   })
+    // }
+   
+  //   var index=4-e.currentTarget.dataset.index;
+  //   for(var i=0;i<=index;i++){
+  //     var item = 'stars['+i+'].flag';
+  //     this.setData({
+  //       [item]:2
+  //     })
+  //   }
+  //   let a=this.data.stars.filter(item=>item.flag==2)
+  //   this.setData({
+  //     chosedStar:6-a.length
+  //   })
+  // },
 
   /**
    * 生命周期函数--监听页面加载
