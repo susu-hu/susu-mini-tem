65b06768aa6fde5246db5b6a6f9a28ac39b4c760
susu1 <1628469970@qq.com>
2021-07-14 01:45:45 +0800

    xx

--- pages/scroll/index.js
+++ pages/scroll/index.js
@@ -5,62 +5,82 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    scrollheight: '',
+    chosed_index:0,
+    cateList:[
+      {
+        id:1,
+        name:'类别1'
+      },
+      {
+        id:2,
+        name:'类别2'
+      },
+      {
+        id:3,
+        name:'类别3'
+      },
+      {
+        id:4,
+        name:'类别4'
+      },
+      {
+        id:5,
+        name:'类别5'
+      },
+      {
+        id:6,
+        name:'类别6'
+      },
+      {
+        id:7,
+        name:'类别7'
+      },
+    ],
+    chosed_cate_id:'',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let windowHeight = wx.getSystemInfoSync().windowHeight // 屏幕的高度
+    let windowWidth = wx.getSystemInfoSync().windowWidth // 屏幕的宽度
+
+    this.setData({
+        scrollheight: windowHeight * 750 / windowWidth - 370
+    })
+    // console.log(windowHeight, this.data.scrollheight)
+    this.setData({
+      chosed_cate_id:this.data.cateList[0].id
+    })
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  
   onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+ 
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+    /**
+     * 选中分类
+     */
+    choseCate(e) {
+      let {id,index} = e.currentTarget.dataset
+      this.setData({
+          chosed_cate_id: id,
+          chosed_index:index,
+          contentActive: 'content' + id
+      }, 
+      // function () {
+      //     //计算位移距离
+      //     this.getRect(`#sidebar-item-${id}`)
+      // }
+      )
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
