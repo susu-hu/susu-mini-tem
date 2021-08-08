49c235382fe41885f217af5ea8234999b2fd3b29
susu <1628469970@qq.com>
2021-08-08 23:28:46 +0800

    新增发送弹幕样式

--- pages/another/Like/index.js
+++ pages/another/Like/index.js
@@ -3,7 +3,25 @@ Page({
 
  
   data: {
-    num: 0
+    //点赞数目
+    num: 0,
+
+    // 弹幕
+    barrage: '',
+    // 弹幕top值
+    scrollTop: 0,
+    // 弹幕内容
+    barrageData: [
+      { name: 'Sam', val: 'Hello World', active: false },
+      { name: 'Sam', val: 'Hello World Hello World Hello World Hello World Hello World Hello World', active: false },
+      { name: 'Sam', val: 'Hello World', active: false },
+      { name: 'Sam', val: 'Hello World', active: false },
+      { name: '管理管理员', val: '欢迎大家', active: true },
+      { name: 'Sam', val: 'Hello World', active: false },
+      { name: 'Sam', val: 'Hello World', active: false },
+      { name: 'Sam', val: 'Hello World', active: false },
+      { name: 'Sam', val: 'Hello World', active: false },
+    ]
   },
 
   onLoad(e) {
@@ -11,8 +29,22 @@ Page({
   },
 
   tapLike() {
-    console.log(1)
-    this.setData({ num: this.data.num+1 })
+    this.setData({ 
+      num: this.data.num+1 
+    })
+  },
+  tapSend() {
+    let that = this
+    that.data.barrageData.push({
+      name: 'Sam',
+      val: that.data.barrage,
+      active: false
+    })
+    that.setData({ barrageData: that.data.barrageData })
+
+    wx.createSelectorQuery().select('#message-item').boundingClientRect(function (rect) {
+      that.setData({ scrollTop: rect.top * 3 });// 拉到最底部
+    }).exec();
   },
 
   
