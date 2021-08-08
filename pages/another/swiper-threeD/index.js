ac0d9b1f45ef5b88027ef0b376fe115681754574
susu <1628469970@qq.com>
2021-08-08 21:41:59 +0800

    新增3d轮播图（swiper）

---
+++ pages/another/swiper-threeD/index.js
@@ -0,0 +1,26 @@
+// pages/another/swiper-threeD/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperIndex: 0,
+  },
+
+  bindchange(e) {
+    this.setData({ swiperIndex: e.detail.current })
+  },
+
+  
+  onLoad: function (options) {
+
+  },
+
+  
+  onShow: function () {
+
+  },
+
+  
+})
