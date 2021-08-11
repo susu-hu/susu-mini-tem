d9c350e38a41fd2946e2d58e691dff8edec7ced5
susu <1628469970@qq.com>
2021-08-11 19:42:58 +0800

    防止swiper卡死

--- pages/another/swiper-threeD/index.js
+++ pages/another/swiper-threeD/index.js
@@ -5,13 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-    swiperIndex: 0,
+    autoplay: true, //是否自动切换
+    interval: 3000, //自动切换时间间隔,3s
+    duration: 500, //  滑动动画时长1s
+    swiperCurrent:0,
+    banner_list: [
+      "https://i.postimg.cc/76br1jzJ/susu1.jpg",
+      'https://i.postimg.cc/WzKK73vQ/20210729155755.jpg',
+      "https://i.postimg.cc/76br1jzJ/susu1.jpg",
+      'https://i.postimg.cc/WzKK73vQ/20210729155755.jpg'
+    ],
   },
 
-  bindchange(e) {
-    this.setData({ 
-      swiperIndex: e.detail.current 
-    })
+  swiperChange(e) {
+    if (e.detail.source == "touch" || e.detail.source == "autoplay") {      
+      this.setData({
+        swiperCurrent: e.detail.current
+      })
+    }
   },
 
   
