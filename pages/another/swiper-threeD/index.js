f4cfb0a2c43d38d86f4bba73c3b1ac33b411a2be
susu <1628469970@qq.com>
2021-08-12 00:42:10 +0800

    新增animation动画效果

--- pages/another/swiper-threeD/index.js
+++ pages/another/swiper-threeD/index.js
@@ -12,8 +12,8 @@ Page({
     banner_list: [
       "https://i.postimg.cc/76br1jzJ/susu1.jpg",
       'https://i.postimg.cc/WzKK73vQ/20210729155755.jpg',
-      "https://i.postimg.cc/76br1jzJ/susu1.jpg",
-      'https://i.postimg.cc/WzKK73vQ/20210729155755.jpg'
+      "https://i.postimg.cc/qRRLS16Q/susu0.jpg",
+      'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
     ],
   },
 
