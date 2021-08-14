242e754eeb57bbb0e58abdcdc6b44d62b1143a09
susu <1628469970@qq.com>
2021-08-15 02:21:19 +0800

    新增动画合成wx.createAnimation

--- pages/subPack/animationBox/index.js
+++ pages/subPack/animationBox/index.js
@@ -5,11 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    img: '',
     animationData1: {},    // 三个动画栈
     animationData2: {},
     animationData3: {}
-
   },
 
   
@@ -19,33 +17,50 @@ Page({
 
   
   onShow: function () {
-    this.animation1 = wx.createAnimation({timingFunction: 'ease', duration: 50})
-    this.animation2 = wx.createAnimation({timingFunction: 'ease', duration: 50})
-    this.animation3 = wx.createAnimation({timingFunction: 'ease', duration: 50})
+    this.init();
+  },
+  init(){
+    this.data.animation1 = wx.createAnimation({timingFunction: 'ease', duration: 50})
+    this.data.animation2 = wx.createAnimation({timingFunction: 'ease', duration: 50})
+    this.data.animation3 = wx.createAnimation({timingFunction: 'ease', duration: 50})
 
     // 初始化
-    this.animation1.opacity(0.2).step()
-    this.animation3.scale(0).step()
+    this.data.animation1.opacity(0.2).step() //scale必须放在opcity的后面
+
+    this.data.animation2.scale(1).step({duration: 2000})
+
+    this.data.animation3.scale(0).step()
 
     // 导出动画栈
-    this.animationData1 = this.animation1.export()
-    this.animationData2 = this.animation2.export()
-    this.animationData3 = this.animation3.export()
+    this.data.animationData1 =  this.data.animation1.export()
+    this.data.animationData2 =  this.data.animation2.export()
+    this.data.animationData3 =  this.data.animation3.export()
+    this.setData({
+      animationData1:this.data.animationData1,
+      animationData2:this.data.animationData2,
+      animationData3:this.data.animationData3
+    })
 
   },
-  compound() {
-    this.animation1.opacity(1).step()
-    this.animation1.scale(0.1).step({duration: 2000, delay: 2900})
+  toPart() {
+    this.data.animation1.opacity(1).step()
+    this.data.animation1.scale(0.1).step({duration: 2000, delay: 2900})
 
-    this.animation2.rotate(360).step({duration: 3000})
-    this.animation2.scale(0.1).step({duration: 2000})
+    this.data.animation2.rotate(360).step({duration: 3000})
+    this.data.animation2.scale(0.1).step({duration: 2000})
 
-    this.animation3.scale(1.2).step({duration: 1000, delay: 4000})
-    this.animation3.scale(1).step({duration: 300})
+    this.data.animation3.scale(1.2).step({duration: 1000, delay: 4000})
+    this.data.animation3.scale(1).step({duration: 300})
     // 导出动画栈
-    this.animationData1 = this.animation1.export()
-    this.animationData2 = this.animation2.export()
-    this.animationData3 = this.animation3.export()
+    this.data.animationData1 =  this.data.animation1.export()
+    this.data.animationData2 =  this.data.animation2.export()
+    this.data.animationData3 =  this.data.animation3.export()
+
+    this.setData({
+      animationData1:this.data.animationData1,
+      animationData2:this.data.animationData2,
+      animationData3:this.data.animationData3
+    })
   },
 
 
