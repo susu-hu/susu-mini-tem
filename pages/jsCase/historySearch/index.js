dc189dd3424d8327893e10df7eebc5c53181b1fb
susu <1628469970@qq.com>
2021-09-22 18:35:00 +0800

    1

--- pages/jsCase/historySearch/index.js
+++ pages/jsCase/historySearch/index.js
@@ -5,62 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    hot_list:['杜甫','李白','李清照','姜子牙','万事如意，心想事成'],
+    list:[],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
+ 
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getSearch(e){
+    let {index}=e.currentTarget.dataset,{hot_list}=this.data;
+    console.log(hot_list[index])
   }
 })
