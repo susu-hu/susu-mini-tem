5fa556d5f710d21a8775231adc3a9c5b377efb7c
susu <1628469970@qq.com>
2021-09-30 17:33:43 +0800

    隐藏微信小程序的小房子

--- pages/cssCase/aButton/index.js
+++ pages/cssCase/aButton/index.js
@@ -1,12 +1,27 @@
 // pages/cssCase/aButton/index.js
 Page({
 
-  data:{
-    ischecked:undefined,
+  data: {
+    ischecked: undefined,
   },
   click() {
     this.setData({
-      ischecked:!this.data.ischecked
+      ischecked: !this.data.ischecked
     })
   },
+  onShow: function () {
+    wx.hideHomeButton({})
+    // wx.hideHomeButton({
+    //   success: function () {
+    //     console.log("hide home success");
+    //   },
+    //   fail: function () {
+    //     console.log("hide home fail");
+    //   },
+    //   complete: function () {
+    //     console.log("hide home complete");
+    //   },
+    // });
+  }
+
 })
