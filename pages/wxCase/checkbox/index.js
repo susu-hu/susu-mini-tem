474d02b9ecedd4dfc2662746826a3d30dac9c299
susu <1628469970@qq.com>
2021-10-17 23:41:12 +0800

    小程序多选框CheckBox

--- pages/wxCase/checkbox/index.js
+++ pages/wxCase/checkbox/index.js
@@ -1,92 +1,76 @@
 Page({
   data: {
-    select_all: false,
-    list:[
+    select_all: false,//是否全选
+    list: [
       {
-        name:'回家的诱惑',
-        price:'888',
-        sell_price:'9.9',
-        star_date:'2021.10.1',
-        end_date:'2021.10.8'
+        name: '回家的诱惑',
+        price: '888',
+        sell_price: '9.9',
+        star_date: '2021.10.1',
+        end_date: '2021.10.8'
       },
       {
-        name:'鱿鱼游戏',
-        price:'888',
-        sell_price:'9.9',
-        star_date:'2021.10.1',
-        end_date:'2021.10.8'
+        name: '鱿鱼游戏',
+        price: '888',
+        sell_price: '9.9',
+        star_date: '2021.10.1',
+        end_date: '2021.10.8'
       },
       {
-        name:'主君的太阳',
-        price:'888',
-        sell_price:'9.9',
-        star_date:'2021.10.1',
-        end_date:'2021.10.8'
+        name: '主君的太阳',
+        price: '888',
+        sell_price: '9.9',
+        star_date: '2021.10.1',
+        end_date: '2021.10.8'
       },
       {
-        name:'地陷',
-        price:'888',
-        sell_price:'9.9',
-        star_date:'2021.10.1',
-        end_date:'2021.10.8'
+        name: '地陷',
+        price: '888',
+        sell_price: '9.9',
+        star_date: '2021.10.1',
+        end_date: '2021.10.8'
       },
       {
-        name:'寄生虫',
-        price:'888',
-        sell_price:'9.9',
-        star_date:'2021.10.1',
-        end_date:'2021.10.8'
+        name: '寄生虫',
+        price: '888',
+        sell_price: '9.9',
+        star_date: '2021.10.1',
+        end_date: '2021.10.8'
       },
       {
-        name:'紧急救援',
-        price:'888',
-        sell_price:'9.9',
-        star_date:'2021.10.1',
-        end_date:'2021.10.8'
+        name: '紧急救援',
+        price: '888',
+        sell_price: '9.9',
+        star_date: '2021.10.1',
+        end_date: '2021.10.8'
       },
       {
-        name:'逆行者',
-        price:'888',
-        sell_price:'9.9',
-        star_date:'2021.10.1',
-        end_date:'2021.10.8'
+        name: '逆行者',
+        price: '888',
+        sell_price: '9.9',
+        star_date: '2021.10.1',
+        end_date: '2021.10.8'
       },
     ],
-    listData: [{
-        code: "1",
-        text: "测试1"
-      },
-      {
-        code: "2",
-        text: "测试2"
-      },
-      {
-        code: "3",
-        text: "测试3"
-      }
-    ],
-    batchIds: '', //选中的ids
+    choseNames: '', //选中的名字列表
   },
 
   //全选与反全选
   selectall: function (e) {
-    console.log(e)
-    var that = this;
     var arr = []; //存放选中id的数组
-    for (let i = 0; i < that.data.listData.length; i++) {
+    for (let i = 0; i < this.data.list.length; i++) {
 
-      that.data.listData[i].checked = (!that.data.select_all)
+      this.data.list[i].checked = (!this.data.select_all)
 
-      if (that.data.listData[i].checked == true) {
+      if (this.data.list[i].checked == true) {
         // 全选获取选中的值
-        arr = arr.concat(that.data.listData[i].code.split(','));
+        arr = arr.concat(this.data.list[i].name.split(','));
       }
     }
-    console.log(arr)
-    that.setData({
-      listData: that.data.listData,
-      select_all: (!that.data.select_all),
-      batchIds: arr
+    this.setData({
+      list: this.data.list,
+      select_all: (!this.data.select_all),
+      choseNames: arr
     })
   },
 
@@ -94,7 +78,16 @@ Page({
   checkboxChange: function (e) {
     console.log(e.detail.value)
     this.setData({
-      batchIds: e.detail.value //单个选中的值
+      choseNames: e.detail.value, //单个选中的值
     })
+    if (this.data.choseNames.length == this.data.list.length) {
+      this.setData({
+        select_all: true
+      })
+    } else {
+      this.setData({
+        select_all: false
+      })
+    }
   },
 })
