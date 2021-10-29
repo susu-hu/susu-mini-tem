d69863dff682e323045507968e380a1e9e4ba276
susu <1628469970@qq.com>
2021-10-29 20:27:29 +0800

    vant组件

---
+++ pages/wxCase/svgCircle/index.js
@@ -0,0 +1,71 @@
+// pages/wxCase/svgCircle/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    value: 25,
+    gradientColor: {
+      '0%': '#ffd01e',
+      '100%': '#ee0a24',
+    },
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
