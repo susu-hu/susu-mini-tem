206c0dee702b5c7980634ebc8992ea22e6883eec
susu <1628469970@qq.com>
2021-11-25 17:46:35 +0800

    'swiper不定长展示数据'

--- pages/wxCase/newsSwiper/index.js
+++ pages/wxCase/newsSwiper/index.js
@@ -1,66 +1,28 @@
 // pages/wxCase/newsSwiper/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+
   data: {
+    data_list:[
+      '测试数据一', '测试数据2', '测试数据3', '测试数据4', '测试数据5', '测试数据6',
+    ],
+    d_num:'',
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  
   onLoad: function (options) {
-
+    let len=this.data.data_list.length;
+    console.log(len)
+    this.setData({
+      d_num:len>2?3:len
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
