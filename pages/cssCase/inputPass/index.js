9162898ba460fc544a48b7ad723c6ee99ed36253
susu <1628469970@qq.com>
2021-11-30 00:22:45 +0800

    1

--- pages/cssCase/inputPass/index.js
+++ pages/cssCase/inputPass/index.js
@@ -2,21 +2,46 @@
 Page({
   data: {
     open: false,//默认不显示密码
-    focus:false,//是否获取焦点
+    open1: false,//默认不显示密码
+    focus: false,//是否获取焦点
+    pass: '',
+    pass1: '',
   },
   switch() {
     this.setData({
       open: !this.data.open
     })
   },
-  focus(){
+  switch1() {
     this.setData({
-      focus:true
+      open1: !this.data.open1
+    })
+    if(this.data.open1){
+      this.setData({
+        pass:this.data.pass1
+      })
+    }else{
+      this.setData({
+        pass:this.data.pass
+      })
+    }
+  },
+  focus() {
+    this.setData({
+      focus: true
     })
   },
-  blur(){
+  blur() {
     this.setData({
-      focus:false
+      focus: false
+    })
+  },
+  getValue(e) {
+    let pass1=JSON.stringify(e.detail.value),
+    pass=e.detail.value.replace(/./g, '*');
+    this.setData({
+      pass1,
+      pass,
     })
   }
 
