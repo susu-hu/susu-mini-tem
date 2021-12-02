cc3d606ceb151ad066695fabb586900e6b5c6100
susu <1628469970@qq.com>
2021-12-02 23:43:53 +0800

    1

--- pages/wxCase/showAction/index.js
+++ pages/wxCase/showAction/index.js
@@ -6,74 +6,106 @@ Page({
   data: {
     hasData: true,
     storeList: [{
-        name: '门店1'
-      },
-      {
-        name: '门店3'
-      },
-      {
-        name: '门店23'
-      },
-      {
-        name: '门店2221'
-      },
-      {
-        name: '门店2222221'
-      }
+      name: '门店1'
+    },
+    {
+      name: '门店3'
+    },
+    {
+      name: '门店23'
+    },
+    {
+      name: '门店2221'
+    },
+    {
+      name: '门店2222221'
+    }
     ],
-    store: '选择门店', //选择的门店
+    store: '选项一', //选择的门店
     themeList: [{
-        name: '主题1'
-      },
-      {
-        name: '主题133'
-      },
+      name: '主题1'
+    },
+    {
+      name: '主题133'
+    },
 
-      {
-        name: '主题12切4234'
-      },
+    {
+      name: '主题12切4234'
+    },
 
-      {
-        name: '2主题1'
-      }, {
-        name: '玩儿主题1'
-      },
+    {
+      name: '2主题1'
+    }, {
+      name: '玩儿主题1'
+    },
     ],
-    theme: '选择主题', //选择的主题
+    theme: '选项二', //选择的主题
     statusList: [{
-        name: '状态一'
-      },
-      {
-        name: '状态234'
-      },
+      name: '状态一'
+    },
+    {
+      name: '状态234'
+    },
 
-      {
-        name: '状态234一'
-      },
+    {
+      name: '状态234一'
+    },
 
-      {
-        name: '状态32423一'
-      }, {
-        name: '状态23423423一'
-      },
+    {
+      name: '状态32423一'
+    }, {
+      name: '状态23423423一'
+    },
     ],
-    status: '选择状态', //选择的状态
+    status: '选项三', //选择的状态
     dateList: [{
-        name: '2021年10月'
-      },
-      {
-        name: '2021年11月'
-      },
-      {
-        name: '2021年12月'
-      },
-      {
-        name: '2021年9月'
-      },
+      name: '2021年10月'
+    },
+    {
+      name: '2021年11月'
+    },
+    {
+      name: '2021年12月'
+    },
+    {
+      name: '2021年9月'
+    },
     ],
     date: '选择时间', //选择的时间
     show_action: false, //是否展示操作
-    list_num: '2', //已选中的个数
+    list_num: 0, //已选中的个数
+    list: [
+      {
+        value: 1,
+        status: 0,
+        disabled: false,
+      },
+      {
+        value: 2,
+        status: 1,
+        disabled: false,
+      },
+      {
+        value: 3,
+        status: 0,
+        disabled: false,
+      },
+      {
+        value: 4,
+        status: 1,
+        disabled: true
+      },
+      {
+        value: 5,
+        status: 0,
+        disabled: true
+      },
+      {
+        value: 6,
+        status: 1,
+        disabled: false
+      },
+    ]
   },
 
   change: function (e) {
@@ -94,21 +126,21 @@ Page({
   },
 
   actionChange(e) {
-    // console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-    // const items = this.data.items
-    // const values = e.detail.value
-    // for (let i = 0, lenI = items.length; i < lenI; ++i) {
-    //   items[i].checked = false
-    //   for (let j = 0, lenJ = values.length; j < lenJ; ++j) {
-    //     if (items[i].value === values[j]) {
-    //       items[i].checked = true
-    //       break
-    //     }
-    //   }
-    // }
-    // this.setData({
-    //   items
-    // })
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+    let { list } = this.data, va = e.detail.value;
+    console.log(va)
+    list.forEach((item, index) => {
+      item.checked = false;
+      va.forEach((i0=>{
+        if(item.value==i0){
+          item.checked = true;
+        }
+      }))
+    })
+    this.setData({
+      list,
+      list_num:va.length
+    })
   },
 
 
