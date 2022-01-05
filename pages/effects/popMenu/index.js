c2ac5d8f219bca832252b3e850fe0541a8ce8b09
susu <1628469970@qq.com>
2022-01-06 00:39:05 +0800

    弹起的menu

---
+++ pages/effects/popMenu/index.js
@@ -0,0 +1,106 @@
+// pages/effects/popMenu/index.js
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    isPopping: false,//是否已经弹出
+    animationPlus: {},//旋转动画
+    animationcollect: {},//item位移,透明度
+    animationTranspond: {},//item位移,透明度
+    animationInput: {},//item位移,透明度
+    //我的博客:http://blog.csdn.net/qq_31383345
+    //CSDN微信小程序开发专栏:http://blog.csdn.net/column/details/13721.html
+  },
+  onLoad: function () {
+
+  },
+  //点击弹出
+  plus: function () {
+    if (this.data.isPopping) {
+      //缩回动画
+      popp.call(this);
+      this.setData({
+        isPopping: false
+      })
+    } else {
+      //弹出动画
+      takeback.call(this);
+      this.setData({
+        isPopping: true
+      })
+    }
+  },
+  input: function () {
+    console.log("input")
+  },
+  transpond: function () {
+    console.log("transpond")
+  },
+  collect: function () {
+    console.log("collect")
+  }
+})
+
+
+
+//弹出动画
+function popp() {
+  //plus顺时针旋转
+  var animationPlus = wx.createAnimation({
+    duration: 500,
+    timingFunction: 'ease-out'
+  })
+  var animationcollect = wx.createAnimation({
+    duration: 500,
+    timingFunction: 'ease-out'
+  })
+  var animationTranspond = wx.createAnimation({
+    duration: 500,
+    timingFunction: 'ease-out'
+  })
+  var animationInput = wx.createAnimation({
+    duration: 500,
+    timingFunction: 'ease-out'
+  })
+  animationPlus.rotateZ(180).step();
+  animationcollect.translate(-100, -100).rotateZ(180).opacity(1).step();
+  animationTranspond.translate(-140, 0).rotateZ(180).opacity(1).step();
+  animationInput.translate(-100, 100).rotateZ(180).opacity(1).step();
+  this.setData({
+    animationPlus: animationPlus.export(),
+    animationcollect: animationcollect.export(),
+    animationTranspond: animationTranspond.export(),
+    animationInput: animationInput.export(),
+  })
+}
+//收回动画
+function takeback() {
+  //plus逆时针旋转
+  var animationPlus = wx.createAnimation({
+    duration: 500,
+    timingFunction: 'ease-out'
+  })
+  var animationcollect = wx.createAnimation({
+    duration: 500,
+    timingFunction: 'ease-out'
+  })
+  var animationTranspond = wx.createAnimation({
+    duration: 500,
+    timingFunction: 'ease-out'
+  })
+  var animationInput = wx.createAnimation({
+    duration: 500,
+    timingFunction: 'ease-out'
+  })
+  animationPlus.rotateZ(0).step();
+  animationcollect.translate(0, 0).rotateZ(0).opacity(0).step();
+  animationTranspond.translate(0, 0).rotateZ(0).opacity(0).step();
+  animationInput.translate(0, 0).rotateZ(0).opacity(0).step();
+  this.setData({
+    animationPlus: animationPlus.export(),
+    animationcollect: animationcollect.export(),
+    animationTranspond: animationTranspond.export(),
+    animationInput: animationInput.export(),
+  })
+}
