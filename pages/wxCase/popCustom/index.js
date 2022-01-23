05385c08abe80200b9ec203991a61327539a2d3c
susu <1628469970@qq.com>
2022-01-23 23:49:17 +0800

    dh

--- pages/wxCase/popCustom/index.js
+++ pages/wxCase/popCustom/index.js
@@ -1,66 +1,27 @@
 // pages/wxCase/popCustom/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+
   data: {
-
+    isShow: true,
+    a: {},
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  show() {
+    this.setData({
+      isShow: false
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  hide() {
+    // var a = wx.createAnimation({
+    //   duration: 500,
+    //   timingFunction: 'ease-out'
+    // })
+    // a.scale(1.08, 1.08).opacity(1).rotateZ(40).scale(0.6).step();
+    this.setData({
+      // a: a.export(),
+      isShow: true
+    })
   }
+
+
 })
