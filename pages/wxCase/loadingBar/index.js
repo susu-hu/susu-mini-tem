7e1b47f57d40dc882829dae2832f5e39c0b76b67
susu <1628469970@qq.com>
2022-02-23 00:54:20 +0800

    1

--- pages/wxCase/loadingBar/index.js
+++ pages/wxCase/loadingBar/index.js
@@ -7,6 +7,8 @@ Page({
   data: {
     scwidth: 0,
     flag: false,
+    tips: '加载中',
+    step: 0
   },
   action() {
     if (!this.data.flag) {
@@ -44,6 +46,44 @@ Page({
   onShow: function () {
 
   },
+  onReady: function () {
+    var me = this;
+    var cxt = wx.createCanvasContext('canvasCircle');
+    cxt.setLineWidth(6);
+    cxt.setStrokeStyle('#eaeaea');
+    cxt.setLineCap('round');
+    cxt.beginPath();
+    cxt.arc(100, 100, 96, 0, 2 * Math.PI, false);
+    cxt.stroke();
+    cxt.draw();
+    //加载动画
+    var steps = 1, startAngle = 1.5 * Math.PI, endAngle = 0, speed = 100, sec = 100;
+    function drawing(s, e) {
+      var context = wx.createCanvasContext('canvasRing');
+      context.setLineWidth(6);
+      context.setStrokeStyle('#11be0f');
+      context.setLineCap('round');
+      context.beginPath();
+      context.arc(100, 100, 96, s, e, false);
+      context.stroke();
+      context.draw();
+    }
+    function drawLoading() {
+      if (steps < 101) {
+        //这里用me,同步数据,渲染页面
+        me.setData({
+          step: steps
+        })
+        endAngle = steps * 2 * Math.PI / speed + startAngle;
+        drawing(startAngle, endAngle);
+        steps++;
+        console.log(steps);
+      } else {
+        clearInterval(this.interval);
+      }
+    }
+    this.interval = setInterval(drawLoading, sec);
+  }
 
 
 })
