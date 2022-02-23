c53a1686f47850efd00ee0af68624230fe1efe70
susu <1628469970@qq.com>
2022-02-23 23:33:46 +0800

    fix

--- pages/wxCase/loadingBar/index.js
+++ pages/wxCase/loadingBar/index.js
@@ -7,7 +7,6 @@ Page({
   data: {
     scwidth: 0,
     flag: false,
-    tips: '加载中',
     step: 0
   },
   action() {
@@ -47,7 +46,7 @@ Page({
 
   },
   onReady: function () {
-    var me = this;
+    var that = this;
     var cxt = wx.createCanvasContext('canvasCircle');
     cxt.setLineWidth(6);
     cxt.setStrokeStyle('#eaeaea');
@@ -61,7 +60,7 @@ Page({
     function drawing(s, e) {
       var context = wx.createCanvasContext('canvasRing');
       context.setLineWidth(6);
-      context.setStrokeStyle('#11be0f');
+      context.setStrokeStyle('orange');
       context.setLineCap('round');
       context.beginPath();
       context.arc(100, 100, 96, s, e, false);
@@ -70,8 +69,7 @@ Page({
     }
     function drawLoading() {
       if (steps < 101) {
-        //这里用me,同步数据,渲染页面
-        me.setData({
+        that.setData({
           step: steps
         })
         endAngle = steps * 2 * Math.PI / speed + startAngle;
