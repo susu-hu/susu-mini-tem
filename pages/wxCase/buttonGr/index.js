5ed12b003b02229d754720e50f4d46b39a28ee1a
susu <1628469970@qq.com>
2022-02-26 01:02:55 +0800

    11

--- pages/wxCase/buttonGr/index.js
+++ pages/wxCase/buttonGr/index.js
@@ -5,14 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    avatar: [
+      'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg',
+      'https://ossweb-img.qq.com/images/lol/web201310/skin/big81005.jpg',
+      'https://ossweb-img.qq.com/images/lol/web201310/skin/big25002.jpg',
+      'https://ossweb-img.qq.com/images/lol/web201310/skin/big91012.jpg'
+    ],
   },
 
   onLoad: function (options) {
-    console.log(this.randomRange(21,23))
+    console.log(this.randomRange(21, 23))
   },
   randomRange(lower, upper) {
     return Math.floor(Math.random() * (upper - lower)) + lower;
   },
- 
+
 })
