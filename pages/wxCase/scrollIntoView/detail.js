f332b5def2d055172a2665ef91024dedc39d69b5
susu <1628469970@qq.com>
2022-03-05 01:12:23 +0800

    sc

--- pages/wxCase/scrollIntoView/detail.js
+++ pages/wxCase/scrollIntoView/detail.js
@@ -5,23 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
+    back: "",
     current: "",
     list: [{
-        back: 'rgb(223, 177, 116)'
-      },
-      {
-        back: 'pink'
-      },
-      {
-        back: 'rgb(121, 60, 70)'
-      },
-      {
-        back: 'rgb(202, 81, 101)'
-      },
-    ]
+      back: 'rgb(223, 177, 116)'
+    },
+    {
+      back: 'pink'
+    },
+    {
+      back: 'rgb(121, 60, 70)'
+    },
+    {
+      back: 'rgb(202, 81, 101)'
+    },
+    ],
+    toView: "",
   },
 
- 
+
   onLoad: function (options) {
     let {
       index
@@ -29,19 +31,28 @@ Page({
     console.log(index)
     if (index != null) {
       this.setData({
-        current: index
+        current: index,
+        back: this.data.list[index].back,
+        toView: 'view' + index
       })
     }
+  },
+  tap(e) {
+    let { index } = e.currentTarget.dataset;
+    this.setData({
+      current: index,
+      back: this.data.list[index].back
+    })
 
   },
 
-  
+
   onShow: function () {
-    let a=Number('1231')
+    let a = Number('1231')
     console.log(a)
-    console.log(typeof(a))
+    console.log(typeof (a))
   },
 
 
- 
+
 })
