d72cebe148c47cc9af56e3891082d368ee2eb651
susu <1628469970@qq.com>
2022-05-07 21:51:03 +0800

    fix

--- pages/jsCase/pageCon/index.js
+++ pages/jsCase/pageCon/index.js
@@ -5,80 +5,85 @@ Page({
    */
   data: {
     contacts: [{
-        id: 1,
-        name: 'Frank',
-        img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
-        phone: '0101 123456',
-        mobile: '0770 123456',
-        email: 'frank@emailionicsorter.com'
-      },
-      {
-        id: 2,
-        name: 'Susan',
-        img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
-        phone: '0101 123456',
-        mobile: '0770 123456',
-        email: 'frank@emailionicsorter.com'
-      },
-      {
-        id: 3,
-        name: 'Emma',
-        img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
-        phone: '0101 123456',
-        mobile: '0770 123456',
-        email: 'frank@emailionicsorter.com'
-      },
-      {
-        id: 4,
-        name: 'Scott',
-        img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
-        phone: '0101 123456',
-        mobile: '0770 123456',
-        email: 'frank@emailionicsorter.com'
-      },
-      {
-        id: 5,
-        name: 'Bob',
-        img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
-        phone: '0101 123456',
-        mobile: '0770 123456',
-        email: 'frank@emailionicsorter.com'
-      },
-      {
-        id: 6,
-        name: 'Olivia',
-        img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
-        phone: '0101 123456',
-        mobile: '0770 123456',
-        email: 'frank@emailionicsorter.com'
-      },
-      {
-        id: 7,
-        name: 'Anne',
-        img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
-        phone: '0101 123456',
-        mobile: '0770 123456',
-        email: 'frank@emailionicsorter.com'
-      },
-      {
-        id: 8,
-        name: 'sunny',
-        img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
-        phone: '0101 123456',
-        mobile: '0770 123456',
-        email: 'frank@emailionicsorter.com'
-      }
+      id: 1,
+      name: 'susu1',
+      img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
+      phone: '0101 123456',
+      mobile: '0770 123456',
+      email: 'frank@emailionicsorter.com'
+    },
+    {
+      id: 2,
+      name: 'susu2',
+      img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
+      phone: '0101 123456',
+      mobile: '0770 123456',
+      email: 'frank@emailionicsorter.com'
+    },
+    {
+      id: 3,
+      name: 'susu3',
+      img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
+      phone: '0101 123456',
+      mobile: '0770 123456',
+      email: 'frank@emailionicsorter.com'
+    },
+    {
+      id: 4,
+      name: 'susu4',
+      img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
+      phone: '0101 123456',
+      mobile: '0770 123456',
+      email: 'frank@emailionicsorter.com'
+    },
+    {
+      id: 5,
+      name: 'susu5',
+      img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
+      phone: '0101 123456',
+      mobile: '0770 123456',
+      email: 'frank@emailionicsorter.com'
+    },
+    {
+      id: 6,
+      name: 'susu6',
+      img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
+      phone: '0101 123456',
+      mobile: '0770 123456',
+      email: 'frank@emailionicsorter.com'
+    },
+    {
+      id: 7,
+      name: 'susu7',
+      img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
+      phone: '0101 123456',
+      mobile: '0770 123456',
+      email: 'frank@emailionicsorter.com'
+    },
+    {
+      id: 8,
+      name: 'susu8',
+      img: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg',
+      phone: '0101 123456',
+      mobile: '0770 123456',
+      email: 'frank@emailionicsorter.com'
+    }
     ],
-    contact:{},
+    contact: {},
     transformIdx: 0,
     position: 'center',
     duration: 300,
     show: false,
     overlay: false
   },
+  onLoad: function (options) {
+    this.setData({
+      contact: this.data.contacts[0]
+    })
+  },
 
   showNext(e) {
-    const idx = e.currentTarget.dataset.idx
+    const idx = e.currentTarget.dataset.idx;
     this.setData({
       show: true,
       contact: this.data.contacts[idx],
@@ -112,11 +117,4 @@ Page({
   },
 
 
-  onLoad: function (options) {
-    this.setData({
-      contact:this.data.contacts[0]
-    })
-  },
-
-  
 })
