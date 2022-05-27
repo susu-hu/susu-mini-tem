ded0dbd9ff698909034c88c07f2fe0d97389ffd6
susu <1628469970@qq.com>
2022-05-27 22:34:25 +0800

    feat:navTab过渡效果

--- pages/subPack/navTab/index.js
+++ pages/subPack/navTab/index.js
@@ -1,78 +1,41 @@
 // pages/subPack/navTab/index.js
 Page({
-
-
   data: {
     navList: ['正在进行', '即将开始', '已结束'],
     nav_type: 0,
+    list: [
+      {
+        color: "linear-gradient(to right, #a8edea 0%, #fed6e3 100%);"
+      },
+      {
+        color: "linear-gradient(to right, #acb6e5 , #86fde8 );"
+      },
+      {
+        color: "linear-gradient(to right, #ed4264 , #ffedbc );"
+      },
+    ],
+    bg_color: 'linear-gradient(to right, #a8edea 0%, #fed6e3 100%)',
+    show: false,
   },
   changeType: function (e) {
     let {
       index
-    } = e.currentTarget.dataset;
+    } = e.currentTarget.dataset, { list } = this.data;
     if (this.data.type === index || index === undefined) {
       return false;
     } else {
       this.setData({
-        nav_type: index
-      })
+        nav_type: index,
+        bg_color: list[index].color,
+        show: true
+      }, setTimeout(() => {
+        this.setData({
+          show: false
+        })
+      }, 600))
     }
-
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
