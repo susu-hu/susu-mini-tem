6d777ff7e273a9752a299899a7f44bece0b5667b
susu <1628469970@qq.com>
2022-05-27 22:18:37 +0800

    fix:菜单项不定长

--- pages/cssCase/menuSwiper/index.js
+++ pages/cssCase/menuSwiper/index.js
@@ -1,8 +1,80 @@
 const app = getApp()
-
+const listMenu = [
+  {
+    name: '菜单1',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单2',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单3',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单4',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单5',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单6',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单7',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单8',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单9',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单10',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单11',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单12',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单13',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单14',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单15',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单16',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+  {
+    name: '菜单17',
+    icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
+  },
+];
 Page({
   data: {
-    lists: []
+    swiperCurrent: 0,//当前所在滑块的 index
+    lists: [],
+    arrLen: 10,//分组数 最大值10,当为奇数时候，默认加1提示为偶数
+    oneLen: 0,//一行展示多少
   },
   onShow() {
     this.getData()
@@ -12,84 +84,19 @@ Page({
    * @param {*} e 
    */
   getData() {
-    let dataList = [
-      {
-        name: '菜单1',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单2',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单3',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单4',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单5',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单6',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单7',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单8',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单9',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单10',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单11',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单12',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单13',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单14',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单15',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单16',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-      {
-        name: '菜单17',
-        icon: 'https://i.postimg.cc/mgsKJGLw/susu1.jpg'
-      },
-    ],
-      arrLen = 8, lists = [];
+    let dataList = listMenu,
+      { arrLen, oneLen } = this.data, lists = [],
+      isOdd = (arrLen % 2) === 0;
+    if (!isOdd && arrLen > 5) arrLen = arrLen + 1;
+    arrLen = arrLen > 10 ? 10 : arrLen;
+    oneLen = arrLen > 5 ? arrLen / 2 : arrLen;
     for (var i = 0; i < dataList.length; i += arrLen) {
       lists.push(dataList.slice(i, i + arrLen));
     }
     this.setData({
-      lists,
+      lists, arrLen,
+      oneLen,
     })
-
   },
   /**
    * 轮播图切换
