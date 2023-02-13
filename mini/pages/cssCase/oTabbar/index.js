4a9e04e0a10830e5e78e98c8f5738d88a3b3be1e
susu <1628469970@qq.com>
2023-02-14 00:44:20 +0800

    fix:tabbar

--- mini/pages/cssCase/oTabbar/index.js
+++ mini/pages/cssCase/oTabbar/index.js
@@ -1,58 +1,39 @@
 // pages/cssCase/nTabbar/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    // 定义菜单
+    menu: [
+      {
+        // 菜单名称
+        name: '三明治',
+        // 阿里巴巴矢量图标库引入图标
+        icon: 'icon-susua-2-53'
+      },
+      {
+        name: '汉堡',
+        icon: 'icon-susuhanbao'
+      },
+      {
+        name: '沙冰',
+        icon: 'icon-susubingsha'
+      },
+      {
+        name: '可乐',
+        icon: 'icon-susukele'
+      },
+    ],
+    // 当前选中菜单的索引，默认为第一个菜单
     currIndex: 0,
-    menu: [{
-        name: "三明治",
-        icon: "icon-susua-2-53"
-      },
-      {
-        name: "汉堡",
-        icon: "icon-susuhanbao"
-      },
-      {
-        name: "冰沙",
-        icon: "icon-susubingsha"
-      },
-      {
-        name: "可乐",
-        icon: "icon-susukele"
-      },
-    ]
   },
+  // 菜单点击事件
   tabClick(e) {
-    let {
-      index
-    } = e.currentTarget.dataset;
+    let { index } = e.currentTarget.dataset;
     this.setData({
-      currIndex: index,
+      currIndex: index
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
+  }
 
 })
