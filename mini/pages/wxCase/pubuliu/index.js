e2184eb0ad1cd1138fbd2c51adb2a3de21966e3d
susu <1628469970@qq.com>
2023-03-20 00:28:14 +0800

    fix

--- mini/pages/wxCase/pubuliu/index.js
+++ mini/pages/wxCase/pubuliu/index.js
@@ -1,50 +1,48 @@
-var app = getApp()
 Page({
   data: {
-    list: [{
+    list: [
+      {
         name: '苏苏',
-        num: '1',
-        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        num: '10',
+        title: '虞美人有大长腿',
         url: 'https://img1.baidu.com/it/u=610726533,1686239110&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=750',
         avatar: 'https://img1.baidu.com/it/u=610726533,1686239110&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=750'
       },
       {
         name: '苏苏6',
-        num: '1',
-        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        num: '8',
+        title: '苏苏哇哈哈-苏苏的bug-苏苏小苏苏',
         url: 'https://img0.baidu.com/it/u=1555018837,2431012166&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
         avatar: 'https://img0.baidu.com/it/u=1555018837,2431012166&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500'
       },
       {
         name: '苏苏2',
-        num: '1',
-        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        num: '12',
+        title: '你好旧时光',
         url: 'https://img2.baidu.com/it/u=2656232546,4221523240&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=777',
         avatar: 'https://img2.baidu.com/it/u=2656232546,4221523240&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=777'
       },
-
       {
         name: '苏苏3',
-        num: '1',
-        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        num: '99+',
+        title: '黑暗荣耀',
         url: 'https://img1.baidu.com/it/u=2602506082,2229028687&fm=253&fmt=auto&app=138&f=JPEG?w=796&h=500',
         avatar: 'https://img1.baidu.com/it/u=2602506082,2229028687&fm=253&fmt=auto&app=138&f=JPEG?w=796&h=500'
       },
       {
         name: '苏苏4',
-        num: '1',
-        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        num: '66',
+        title: '归路',
         url: 'https://img1.baidu.com/it/u=3462460603,3220043498&fm=253&fmt=auto&app=120&f=JPEG?w=690&h=458',
         avatar: 'https://img1.baidu.com/it/u=3462460603,3220043498&fm=253&fmt=auto&app=120&f=JPEG?w=690&h=458'
       },
       {
         name: '苏苏5',
-        num: '1',
-        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        num: '8',
+        title: '花琉璃',
         url: 'https://img0.baidu.com/it/u=3184989322,3773788620&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=667',
         avatar: 'https://img0.baidu.com/it/u=3184989322,3773788620&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=667'
       },
-
     ]
   }
 })
