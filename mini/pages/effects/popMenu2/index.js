b0d7b3fa52533deaaaeff781eee35cac7db37761
susu <1628469970@qq.com>
2023-03-25 00:02:20 +0800

    style

--- mini/pages/effects/popMenu2/index.js
+++ mini/pages/effects/popMenu2/index.js
@@ -2,47 +2,43 @@
 Page({
 
   data: {
-    isShow: true,
+    hidden: true,//是否隐藏菜单
+    // 菜单列表
     menuList: [
       {
-        name: '菜单一',
+        name: "菜单一",
         src: 'https://i.postimg.cc/Bn1XpkSn/susu.jpg',
         url: ''
       },
       {
-        name: '菜单二',
+        name: "菜单二",
         src: 'https://i.postimg.cc/Gm7KjGmN/111.jpg',
         url: ''
       },
       {
-        name: '菜单三',
+        name: "菜单三",
         src: 'https://i.postimg.cc/Bv28vfkg/222.webp',
         url: ''
       },
       {
-        name: '菜单四',
+        name: "菜单四",
         src: 'https://i.postimg.cc/65STLQNc/333.webp',
         url: ''
       },
-    ]
+      {
+        name: "菜单6",
+        src: 'https://i.postimg.cc/Bn1XpkSn/susu.jpg',
+        url: ''
+      },
+    ],
+    className: '',//样式名称
   },
-  click() {
+
+  handleClick() {
+    let { hidden } = this.data;
     this.setData({
-      isShow: !this.data.isShow
+      hidden: !hidden,
+      className: !hidden ? 'hide' : 'show'
     })
-    if (this.data.isShow) {
-      this.setData({
-        maskAnimation: 'maskClose',
-        btnAnimation: 'menuClose'
-      })
-    } else {
-      this.setData({
-        maskAnimation: 'maskOpen',
-        btnAnimation: 'menuOpen'
-      })
-    }
-  },
-  preventdefault() {
-    return false;
   }
 })
