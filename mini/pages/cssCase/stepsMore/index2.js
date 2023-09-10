3e76bc4951835f5bef0f149b265620398fef612e
susu <1628469970@qq.com>
2023-09-10 23:15:54 +0800

    feat:steps more

---
+++ mini/pages/cssCase/stepsMore/index2.js
@@ -0,0 +1,148 @@
+// pages/cssCase/stepsMore/index2.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [
+      [{
+          status: 1, //1已完成 0 当前步骤 -1 未开始
+          text: "已完成",
+        },
+        {
+          status: 0,
+          text: "当前步骤",
+        },
+      ],
+      [{
+          status: 1, //1已完成 0 当前步骤 -1 未开始
+          text: "已完成",
+        },
+        {
+          status: 0,
+          text: "当前步骤",
+        },
+        {
+          status: -1,
+          text: "未完成",
+        },
+      ],
+      [{
+          status: 1, //1已完成 0 当前步骤 -1 未开始
+          text: "已完成",
+        },
+        {
+          status: 1,
+          text: "当前步骤",
+        },
+        {
+          status: 0,
+          text: "未完成",
+        },
+        {
+          status: -1,
+          text: "未完成",
+        },
+      ],
+      [{
+          status: 1, //1已完成 0 当前步骤 -1 未开始
+          text: "已完成",
+        },
+        {
+          status: 1,
+          text: "已完成",
+        },
+        {
+          status: 0,
+          text: "当前步骤",
+        },
+        {
+          status: -1,
+          text: "未完成",
+        },
+        {
+          status: -1,
+          text: "未完成",
+        },
+      ],
+      [{
+          status: 1, //1已完成 0 当前步骤 -1 未开始
+          text: "已完成",
+        },
+        {
+          status: 1,
+          text: "已完成",
+        },
+        {
+          status: 1,
+          text: "已完成",
+        },
+        {
+          status: 1,
+          text: "已完成",
+        },
+        {
+          status: 0,
+          text: "当前步骤",
+        },
+        {
+          status: -1,
+          text: "未完成",
+        },
+      ],
+      [{
+          status: 1, //1已完成 0 当前步骤 -1 未开始
+          text: "已完成",
+        },
+        {
+          status: 1,
+          text: "已完成",
+        },
+        {
+          status: 1,
+          text: "已完成",
+        },
+        {
+          status: 1,
+          text: "未完成",
+        },
+        {
+          status: 0,
+          text: "当前步骤",
+        },
+        {
+          status: -1,
+          text: "未完成",
+        },
+      ],
+      [{
+        status: 1, //1已完成 0 当前步骤 -1 未开始
+        text: "已完成",
+      },
+      {
+        status: 1,
+        text: "已完成",
+      },
+      {
+        status: 1,
+        text: "已完成",
+      },
+      {
+        status: 1,
+        text: "已完成",
+      },
+      {
+        status: 1,
+        text: "已完成",
+      },
+      {
+        status: 1,
+        text: "已完成",
+      },
+    ],
+    ],
+  },
+
+
+})
